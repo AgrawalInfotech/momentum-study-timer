4325ad10063ca82d91b0051d2ff5eb9994ddcaf9
AgrawalInfotech <agansh1603@gmail.com>
2026-05-14 19:09:33 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'momentum-app-v1';
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
